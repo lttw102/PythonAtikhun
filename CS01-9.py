e56880ce957025abbc265ee781a974834642f18c
TAWANTK <43932@virtual.prc.ac.th>
2022-09-24 21:05:39 +0700

    Create CS01-9.py

---
+++ CS01-9.py
@@ -0,0 +1,8 @@
+Number1 = float(input("Input your Number,1 : "))
+Number2 = float(input("Input your Number,2 : "))
+Number3 = float(input("Input your NuMber,3 : "))
+
+if Number1 > Number2 and Number1 < Number3 :
+    print("Conditions is True")
+else :
+    print("Conditions is False")
