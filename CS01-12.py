80690697dfcc86f8f10dd2103a76b6948029801f
TAWANTK <43932@virtual.prc.ac.th>
2022-09-24 21:05:53 +0700

    Create CS01-12.py

---
+++ CS01-12.py
@@ -0,0 +1,11 @@
+f = ["apple", "mange", "avocado", "malagor"]
+for x in f:
+    if x == "apple":
+        print(x)
+
+print("-"*30)
+
+for x in range(10):
+    print(x)
+    for y in range(10):
+        print(y)
